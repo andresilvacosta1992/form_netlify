aa435f7c5b8f705b5e8d0fbbf8bc460026aded31
André Costa <andresilvacosta1992@gmail.com>
2023-05-25 15:05:38 -0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 var urlAtual = window.location.href;
-document.getElementById('url_atual').value = urlAtual;
-document.getElementById('span-teste').value = "alkdjfsadf";
+//document.getElementById('url_atual').value = urlAtual;
+//document.getElementById('span-teste').value = "alkdjfsadf";
+
+
+//input-hidden = '<input type="text" name="informacao_oculta" value="' . urlAtual . '">';
+
+//var input_hidden = 'urlAtual';
+var teste = 'teste';
+
+var meuComponente = document.getElementById('hidden-url');
+meuComponente.innerHTML = '<input type="hidden" name="url" value="' + urlAtual  + '">';
